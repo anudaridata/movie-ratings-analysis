e599512f3a83ffbeecbec49c9591c04f57b39ad3
Anudari Chingunbat <164714826+anudaridata@users.noreply.github.com>
2025-10-12 11:44:47 +0900

    Create 01_exploration.sql

---
+++ queries/01_exploration.sql
@@ -0,0 +1,11 @@
+-- MovieLens Dataset Exploration
+-- Author: Anudari Chingunbat
+-- Date: October 2025
+
+-- TODO: Add exploration queries here after importing data
+
+-- Question 1: How many movies are in the dataset?
+
+-- Question 2: How many ratings are there?
+
+-- Question 3: What's the date range of the ratings?
