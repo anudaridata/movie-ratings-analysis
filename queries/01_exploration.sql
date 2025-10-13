a9f7b0afdbc262a1f26f0787c571410452f16364
Anudari Chingunbat <164714826+anudaridata@users.noreply.github.com>
2025-10-13 22:01:44 +0900

    Update 01_exploration.sql

--- queries/01_exploration.sql
+++ queries/01_exploration.sql
@@ -2,10 +2,14 @@
 -- Author: Anudari Chingunbat
 -- Date: October 2025
 
--- TODO: Add exploration queries here after importing data
+-- Question 1: What are the 10 most rated movies? 
+-- This question is asking about popularity of the movie (most popular movies gets rated the most) and not about movies with highest score! 
+SELECT movies.title, COUNT(*) as rating_count
+FROM ratings
+INNER JOIN movies on movies.movie_id = ratings.movie_id
+GROUP BY movies_title
+ORDER BY rating_count DESC
+LIMIT 10;
+-- Results (HINT: It is connected to spaces!) was Star Wars (1977) was the most rated with 583 ratings, 
+-- followed by Contact (1997) with 509 ratings.
 
--- Question 1: How many movies are in the dataset?
-
--- Question 2: How many ratings are there?
-
--- Question 3: What's the date range of the ratings?
