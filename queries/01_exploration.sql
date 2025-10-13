7be872fb129184d49caea6c700ebe42eb8a1d59f
Anudari Chingunbat <164714826+anudaridata@users.noreply.github.com>
2025-10-13 23:08:08 +0900

    Update 01_exploration.sql

--- queries/01_exploration.sql
+++ queries/01_exploration.sql
@@ -12,4 +12,12 @@ ORDER BY rating_count DESC
 LIMIT 10;
 -- Results (HINT: It is connected to spaces!) was Star Wars (1977) was the most rated with 583 ratings, 
 -- followed by Contact (1997) with 509 ratings.
+-- Question 2: Which 10 movies have the highest average rating, considering only movies at least 50 ratings? Limit the answer to 10
+SELECT movies.title, COUNT(*) as rating_count, ROUND(AVG(ratings.rating), 2) as average_rating
+FROM ratings
+INNER JOIN movies on ratings.movie_id = movies.movie_id
+GROUP BY movies.title
+HAVING COUNT(*)>50
+ORDER BY average_rating DESC
+LIMIT 10;
 
